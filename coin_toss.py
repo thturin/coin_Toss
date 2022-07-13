db1dc99ab85c08c3853334540d8a950ad938b791
Tatiana Turin <tatiana.turin@gmail.com>
2022-07-12 22:18:43 -0400

    c

--- coin_toss.py
+++ coin_toss.py
@@ -1,5 +1,10 @@
-"""Project Coin Toss"""
-import pygame
+"""Project Coin Toss
+Fox and coin appear on the screen. Use the arrow keys to move the fox toward the coin. If the fox touches coin, 10 points given.
+Duration of game 7 seconds and total score is shown
+
+
+"""
+
 import pgzrun
 
 
