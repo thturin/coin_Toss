92af837ebef006ff156113bf32ef3e031579e20f
Tatiana Turin <tatiana.turin@gmail.com>
2022-07-13 14:45:25 -0400

    c

--- coin_toss_update.py
+++ coin_toss_update.py
@@ -6,17 +6,25 @@ hacks and tweaks
 extra time
 go faster
 
+
+countdown clock
+add seconds to the countdown clock when you get a coin
+WHAT IS THE PROBLEM ONCE YOU DO THE COUNTDOWN CLOCK CORRECTLY? ---> THE GAME ENDS AT THE INITIAL TIME GIVEN FOR TIME_UP
+
 have the coins move
 
 """
 
-import pgzrun
-import random
+import pgzrun, random, pygame
 
 WIDTH = 400
 HEIGHT = 400
 score = 0
 game_over = False
+time_left = 7
+timer = 0
+timer2_check = 0
+timer2 = 0
 
 fox = Actor("fox")
 fox.pos = 100,100
@@ -25,10 +33,16 @@ coin = Actor("coin")
 coin.pos = 200,200
 
 def draw():
+    global timer
     screen.fill("dark green")
     fox.draw()
     coin.draw()
+
     screen.draw.text("Score: "+ str(score), color="black", topleft=(10,10))
+
+    #timer = int(pygame.time.get_ticks() / 1000) # gives you the time in ms since the program was started
+
+    screen.draw.text("Time Remaining: {}".format(time_left), color="black", topright=(290, 10))
 
     if game_over:
         screen.fill("pink")
@@ -42,8 +56,19 @@ def time_up():
     global game_over
     game_over = True
 
+
 def update(): #built in pygame zero function. Once you've defined it, pygame zero will run it automatically
     global score
+    global time_left
+    global timer2, timer2_check
+    timer2_check+=1
+
+    if timer2_check%60==0: #1 second has passed
+        #subtract 1 from our countdown time_left var
+        time_left-=1
+        timer2+=1
+        #print(" time : {}".format(timer2))
+       # print(time_left)
 
     if keyboard.left:
         fox.x -= 2
@@ -59,10 +84,20 @@ def update(): #built in pygame zero function. Once you've defined it, pygame zer
     if coin_collected: #if the fox reaches the coin, add a point
         score+=10
         place_coin() #place coin somwhere
+        time_left +=3
+
+    clock.unschedule(time_up)
+    clock.schedule(time_up,time_left)
 
 
+"""
+the clock.schedule(callback, delay) <- the delay variable will not be changed since when running this code, it will be called and put into memory initially with original time_up value. 
+It will not be called again. This is why we put the clock.schedule() method into the update() function itself. PG Zero calls this function 60x every 1 second so what we do is unschedule and schedule it
+each iteration which means the updated time_left variable is set as a parameter for the newly called clock.schedule() method
+"""
 
-clock.schedule(time_up, 7.0) #runs the function time_up() 7 seconds after the game starts
+
+#clock.schedule(time_up,time_left) #runs the function time_up() 7 seconds after the game starts
 
 
 place_coin()
